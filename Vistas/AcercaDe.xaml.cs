9db4c94748683b295b9093e86c7f591459e045b7
john silvestre <johnkerlin52@gmail.com>
2025-03-20 17:49:37 -0400

    feat: about me

---
+++ Vistas/AcercaDe.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace AppMultasDigesett.Vistas;
+
+public partial class AcercaDe : ContentPage
+{
+	public AcercaDe()
+	{
+		InitializeComponent();
+        AgenteFoto.Source = "miimagen.jpg";
+        NombreLabel.Text = "John Kerlin Silvestre";
+        MatriculaLabel.Text = "20231192";
+        FraseLabel.Text = "🚸 La seguridad vial no es solo una responsabilidad, es un compromiso con la vida.";
+    }
+}
